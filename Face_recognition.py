c46edebc51227b116b3486b17af7ecee33d4681e
Satya Prakash Mohanty <mebubu03@gmail.com>
2023-10-18 01:08:02 +0530

    made a time period

--- Face_recognition.py
+++ Face_recognition.py
@@ -4,6 +4,7 @@ import numpy as np
 import csv
 import os
 from datetime import datetime
+import time
 
 video_capture = cv2.VideoCapture(0)
 
@@ -21,7 +22,6 @@ known_face_encoding = [
 known_face_names = [
     "bubu",
     "dida"
-
 ]
 
 students = known_face_names.copy()
@@ -34,38 +34,47 @@ s = True
 now = datetime.now()
 current_date = now.strftime("%Y-%m-%d")
 
-
 f = open(current_date+'.csv','w+', newline='')
 lnwriter = csv.writer(f)
 
-while True:
-    _,frame = video_capture.read()
-    small_frame = cv2.resize(frame,(0,0),fx=0.25, fy=0.25)
-    rgb_small_frame = small_frame[:,:,::-1]
-    if s:
-        face_location = face_recognition.face_locations(rgb_small_frame)
-        face_encodings = face_recognition.face_encodings(rgb_small_frame)
-        face_names = []
-        for face_encoding in face_encodings:
-            matches = face_recognition.compare_faces(known_face_encoding, face_encoding)
-            name = ""
-            face_distance = face_recognition.face_distance(known_face_encoding, face_encoding)
-            best_match_index = np.argmin(face_distance)
-            if matches[best_match_index]:
-                name = known_face_names[best_match_index]
+# Set start and end times for detection period (in seconds)
+start_time = time.time()
+end_time = start_time + 60  # 1 hour later
 
-            face_names.append(name)
-            if name in known_face_names:
-                if name in students:
-                    students.remove(name)
-                    print(students)
-                    current_time = now.strftime("%H-%M-%S")
-                    lnwriter.writerow([name,current_time])
-    cv2.imshow("attendence system", frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+while True:
+    current_time = time.time()
+    if current_time < end_time:
+        _,frame = video_capture.read()
+        small_frame = cv2.resize(frame,(0,0),fx=0.25, fy=0.25)
+        rgb_small_frame = small_frame[:,:,::-1]
+        if s:
+            face_location = face_recognition.face_locations(rgb_small_frame)
+            face_encodings = face_recognition.face_encodings(rgb_small_frame)
+            face_names = []
+            for face_encoding in face_encodings:
+                matches = face_recognition.compare_faces(known_face_encoding, face_encoding)
+                name = ""
+                face_distance = face_recognition.face_distance(known_face_encoding, face_encoding)
+                best_match_index = np.argmin(face_distance)
+                if matches[best_match_index]:
+                    name = known_face_names[best_match_index]
+
+                face_names.append(name)
+                if name in known_face_names:
+                    if name in students:
+                        students.remove(name)
+                        print(students)
+                        current_time_str = now.strftime("%H-%M-%S")
+                        lnwriter.writerow([name,current_time_str])
+        cv2.imshow("attendance system", frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    else:
+        # After end time, mark remaining students as absent
+        for student in students:
+            lnwriter.writerow([student, 'Absent'])
         break
 
 video_capture.release()
 cv2.destroyAllWindows()
 f.close()
-
